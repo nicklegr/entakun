10882cff4b521f44025131c8182bac518a98cf79
Nickle <nickle-github@nickle.ath.cx>
2013-02-25 22:58:35 +0900

    Issue #1を再現するテストを追加

--- spec/task_open_close_spec.rb
+++ spec/task_open_close_spec.rb
@@ -5,6 +5,9 @@ require 'task_helper'
 SHORT_NAME = 'hoge'
 LONG_NAME = 'hoge' * 20
 
+SHORT_URL = 'http://example.com/'
+LONG_URL = 'http://example.com/dummy/very/very/very/very/very/very/very/long/url'
+
 feature 'Task open/close', js: true do
   background do
     DatabaseCleaner.clean
@@ -100,5 +103,23 @@ feature 'Task open/close', js: true do
     expect(task.close?).to be_true
     expect(task.name).not_to eq(LONG_NAME)
     expect(task.truncated?).to be_true
+  end
+
+  scenario 'Short URL linked' do
+    task = Test::Task.first(page)
+    task.edit(SHORT_URL)
+
+    expect(task.truncated?).not_to be_true
+    expect(task.elem).to have_link(SHORT_URL, href: SHORT_URL)
+  end
+
+  scenario 'Long URL truncated, but href is not truncated' do
+    task = Test::Task.first(page)
+    task.edit(LONG_URL)
+
+    link_text = task.name.gsub(/\.\.\.$/, '')
+
+    expect(task.truncated?).to be_true
+    expect(task.elem).to have_link(link_text, href: LONG_URL)
   end
 end
